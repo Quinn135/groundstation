70e511ee4c8a4342742039c2c0488d13eb5c5142
QuinnC <cooperstein.quinn@gmail.com>
2025-04-11 23:54:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -92,69 +92,73 @@ function App() {
         ></Recenter>
         <Marker position={[data.lat, data.lng]}></Marker>
       </MapContainer>
-      <div className="flex flex-row flex-wrap items-center justify-between gap-3 p-2">
-        <p className="bg-neutral-800 p-1 px-2.5 rounded">
-          {data.alt.toFixed(2)}ft
-        </p>
-        <p className="bg-neutral-800 p-1 px-2.5 rounded">
-          {data.mps.toFixed(2)}m/s
-        </p>
-        <p className="bg-neutral-800 p-1 px-2.5 rounded">
-          {(data.mps * 2.236936).toFixed(2)}mph
-        </p>
-        <p className="bg-neutral-800 p-1 px-2.5 rounded">
-          {` ${data.time.toString().slice(-8, -6)}:${data.time
-            .toString()
-            .slice(-6, -4)}:${data.time.toString().slice(-4, -2)} `}
-          UTC (
-          {`${
-            Number(data.time.toString().slice(-8, -6)) - 5 < 0
-              ? Number(data.time.toString().slice(-8, -6)) - 5 + 24
-              : Number(data.time.toString().slice(-8, -6)) - 5
-          }:${data.time.toString().slice(-6, -4)}:${data.time
-            .toString()
-            .slice(-4, -2)}`}{" "}
-          our time)
-        </p>
-        <p className="bg-neutral-800 p-1 px-2.5 rounded">{data.sats} Sats</p>
-        <button
-          className="cursor-pointer bg-neutral-800 p-1 px-2.5 rounded active:bg-neutral-700"
-          onClick={() => {
-            if (
-              mapUrl == "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            ) {
-              setMapUrl(
-                "http://services.arcgisonline.com/ArcGis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
-              );
-            } else {
-              setMapUrl("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
-            }
-          }}
-        >
-          Layer
-        </button>
-        <label
-          className="cursor-pointer bg-neutral-800 p-1 px-2.5 rounded active:bg-neutral-700"
-          htmlFor="autoCenter"
-        >
-          Auto Center&nbsp;
-          <input
-            type="checkbox"
-            name="Auto Center"
-            id="autoCenter"
-            className="cursor-pointer"
-            onChange={(e) => {
-              setAutoCenter(e.target.checked);
+      <div className="flex flex-col">
+        <div className="flex flex-row flex-wrap items-center justify-start gap-3 p-2">
+          <p className="bg-neutral-800 p-1 px-2.5 rounded">
+            {data.alt.toFixed(2)}ft
+          </p>
+          <p className="bg-neutral-800 p-1 px-2.5 rounded">
+            {data.mps.toFixed(2)}m/s
+          </p>
+          <p className="bg-neutral-800 p-1 px-2.5 rounded">
+            {(data.mps * 2.236936).toFixed(2)}mph
+          </p>
+          <p className="bg-neutral-800 p-1 px-2.5 rounded">
+            {` ${data.time.toString().slice(-8, -6)}:${data.time
+              .toString()
+              .slice(-6, -4)}:${data.time.toString().slice(-4, -2)} `}
+            UTC (
+            {`${
+              Number(data.time.toString().slice(-8, -6)) - 5 < 0
+                ? Number(data.time.toString().slice(-8, -6)) - 5 + 24
+                : Number(data.time.toString().slice(-8, -6)) - 5
+            }:${data.time.toString().slice(-6, -4)}:${data.time
+              .toString()
+              .slice(-4, -2)}`}{" "}
+            our time)
+          </p>
+          <p className="bg-neutral-800 p-1 px-2.5 rounded">{data.sats} Sats</p>
+        </div>
+        <div className="flex flex-row flex-wrap items-center justify-end gap-3 p-2">
+          <button
+            className="cursor-pointer bg-neutral-800 p-1 px-2.5 rounded active:bg-neutral-700"
+            onClick={() => {
+              if (
+                mapUrl == "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              ) {
+                setMapUrl(
+                  "http://services.arcgisonline.com/ArcGis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
+                );
+              } else {
+                setMapUrl("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+              }
             }}
-            defaultChecked
-          />
-        </label>
-        <button
-          id={"connectButton"}
-          className="cursor-pointer bg-neutral-800 p-1 px-2.5 rounded active:bg-neutral-700"
-        >
-          Connect
-        </button>
+          >
+            Layer
+          </button>
+          <label
+            className="cursor-pointer bg-neutral-800 p-1 px-2.5 rounded active:bg-neutral-700"
+            htmlFor="autoCenter"
+          >
+            Auto Center&nbsp;
+            <input
+              type="checkbox"
+              name="Auto Center"
+              id="autoCenter"
+              className="cursor-pointer"
+              onChange={(e) => {
+                setAutoCenter(e.target.checked);
+              }}
+              defaultChecked
+            />
+          </label>
+          <button
+            id={"connectButton"}
+            className="cursor-pointer bg-neutral-800 p-1 px-2.5 rounded active:bg-neutral-700"
+          >
+            Connect
+          </button>
+        </div>
       </div>
     </div>
   );
